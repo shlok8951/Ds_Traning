d7f39cc668a5f82316901b476272c897b65369ee
shlok8951 <shlokj771@gmail.com>
2026-05-27 09:23:58 +0530

    Add files via upload

---
+++ Day_2.py
@@ -0,0 +1,57 @@
+# a = 10
+# print(a)
+# print(id(a))
+# b = 10
+# print(b)
+# print(id(b))
+
+
+# a = 10
+# b = 10.2
+# c = "Shlok"
+# d = True
+# e = 'a'
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+
+# num = input("Enter a number :")
+# print(id(num))
+# num = int(num)  
+# print(id(num))  # addresses will change when we change the type of variable
+# print(num*5)
+# print(type(num))
+
+# a = 6
+# b = 20
+# print(id(a))
+# print(id(b))
+# print(id(a+b))
+
+# x = int(input("Enter the number :"))
+# if(x%2==0):
+#     print("Even")
+# else :
+#     print("Odd")    
+
+# n1 = 10
+# n2 = 20
+# print(n1!=n2)
+
+# n1 = 10
+# n1 += 3
+# print(n1)
+
+# n2 = 4
+# n2 -= 2
+# print(n2)
+
+# n3 = 3
+# n3 **= 2
+# print(n3)
+
+a = 0
+b = 20
+print(a or b)
